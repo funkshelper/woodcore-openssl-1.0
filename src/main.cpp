d8a56478a341fd697a9cf0715cf0ab1b2df754e2
Paro <pamenarti@gmail.com>
2022-09-09 21:44:01 +0300

    int64_t nSubsidy =
    int64_t nSubsidy =

--- src/main.cpp
+++ src/main.cpp
@@ -1722,7 +1722,7 @@ bool ReadBlockFromDisk(CBlock& block, const CBlockIndex* pindex, const Consensus
 
 CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
 {
-    int64_t nSubsidy = 1000000 * COIN;
+    int64_t nSubsidy = 100000 * COIN;
     //CAmount nSubsidy = 1000000 * COIN;
     if (nHeight<100) return 1*COIN;	
     else return nSubsidy/nHeight;
